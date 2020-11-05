643cdb2d2c89ccafa5da6fac3143da30304b59b9
martel <benjamin.martel.etu@univ-lille.fr>
2020-11-05 12:14:26 +0100

    RecyclerView created

--- Android-API-eService/app/src/main/java/com/example/android_api_eservice/MainActivity.java
+++ Android-API-eService/app/src/main/java/com/example/android_api_eservice/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.android_api_eservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> names = new ArrayList<>();
+    private List<String> imageUrls = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initImageBitmaps();
+    }
+
+
+    //default images to test
+    private void initImageBitmaps(){
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
+        names.add("bulbasaur");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png");
+        names.add("ivysaur");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png");
+        names.add("venusaur");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png");
+        names.add("charmander");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png");
+        names.add("charmeleon");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png");
+        names.add("charizard");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png");
+        names.add("squirtle");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png");
+        names.add("wartortle");
+
+        imageUrls.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png");
+        names.add("blastoise");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(names, imageUrls,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ Android-API-eService/app/src/main/java/com/example/android_api_eservice/RecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package com.example.android_api_eservice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private List<String> imageNames = new ArrayList<>();
+    private List<String> images = new ArrayList<>();
+    private Context context;
+
+    public RecyclerViewAdapter(List<String> imageNames, List<String> images, Context context) {
+        this.imageNames = imageNames;
+        this.images = images;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Glide.with(context)
+                .asBitmap()
+                .load(images.get(position))
+                .into(holder.image);
+
+        holder.imageName.setText(imageNames.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(context, imageNames.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
