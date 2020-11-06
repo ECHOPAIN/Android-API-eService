a7787cededd99c162a13a07cf5339a95dfe87818
martel <benjamin.martel.etu@univ-lille.fr>
2020-11-06 11:55:45 +0100

    adding grid view

--- Android-API-eService/app/src/main/java/com/example/android_api_eservice/MainActivity.java
+++ Android-API-eService/app/src/main/java/com/example/android_api_eservice/MainActivity.java
@@ -1,25 +1,53 @@
 package com.example.android_api_eservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.android_api_eservice.adapters.RecyclerViewAdapter;
 import com.example.android_api_eservice.models.Pokemon;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FloatingActionButton fab;
+    private RecyclerView recyclerView;
     private List<Pokemon> pokemons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        // set a GridLayoutManager with 3 number of columns , vertical gravity and false value for reverseLayout to show the items from start to end
+        final GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),3,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if (recyclerView.getLayoutManager().equals(gridLayoutManager)) {
+                    recyclerView.setLayoutManager(linearLayoutManager);
+                    fab.setImageResource(R.drawable.grid_display);
+                }else{
+                    recyclerView.setLayoutManager(gridLayoutManager);
+                    fab.setImageResource(R.drawable.list_display);
+                }
+            }
+        });
 
         initImageBitmaps();
     }
